3c4ceabbe45079fefc54fbf71f1f670d29ae6651
Darling <dnomar165@gmail.com>
2021-10-01 04:11:58 -0400

    I changed the color of shadows and background

--- src/Components/styledMainContent.js
+++ src/Components/styledMainContent.js
@@ -16,7 +16,7 @@ export const ProjectsCard = styled.div`
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
     background: white;
     border-radius: 5px;
-    box-shadow: rgba(0, 0, 250, 0.4) 0px 2px 4px, rgba(0, 0, 250, 0.3) 0px 7px 13px -3px, rgba(0, 0, 250, 0.2) 0px -3px 0px inset;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
     width:95%;
     height: 200px;
     margin: 10px;
@@ -85,10 +85,10 @@ export const ProjectsLink = styled.button`
     text-decoration: none;
     color: black;
     grid-row: 3/4;
-    box-shadow: rgba(0, 0, 250, 0.4) 0px 2px 4px, rgba(0, 0, 250, 0.1) 0px 7px 13px -3px, rgba(0, 0, 250, 0.1) 0px -3px 0px inset;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.1) 0px 7px 13px -3px, rgba(0, 0, 0, 0.1) 0px -3px 0px inset;
 
     &:hover{
         cursor: pointer;
-        box-shadow: rgba(178, 34, 34, 0.7) 0px 2px 4px, rgba(178, 34, 34, 0.4) 0px 7px 13px -3px, rgba(178, 34, 34, 0.2) 0px -3px 0px inset; 
+        box-shadow: rgba(0, 0, 0, 0.7) 0px 2px 4px, rgba(0, 0, 0, 0.4) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset; 
    }
 `;
